37bfb8780601d85f1391dca3d4a17e56fb18dd9a
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-01-08 14:35:52 +0100

    Initial Commit

---
+++ ReadSpeedster/ReadSpeedsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadSpeedsterApp.swift
+//  ReadSpeedster
+//
+//  Created by Andreas Antonsson on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct ReadSpeedsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
